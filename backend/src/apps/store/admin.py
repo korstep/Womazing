70ee768221ebcd7160bb336504fe701d0b3ea3af
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-03 20:47:26 +0300

    Created models and add to admin panel

--- backend/src/apps/store/admin.py
+++ backend/src/apps/store/admin.py
@@ -1,3 +1,59 @@
+from .models import *
+from django.utils.text import slugify
+from unidecode import unidecode
 from django.contrib import admin
 
+
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    prepopulated_fields = {'slug': ("name",)}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'old_price', 'category')
+    list_display_links = ('id', 'name')
+    prepopulated_fields = {'slug': ('name',)}
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        obj.slug = slugify(f"{obj.id}-{unidecode(obj.name)}")
+        super().save_model(request, obj, form, change)
+
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'size')
+
+
+class ProductSizeColorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'color', 'size', 'quantity')
+    list_filter = ('product', 'color', 'size', 'quantity')
+    search_fields = ('product__name',)
+
+    # def get_images(self, obj):
+    #     images = obj.image_url.all()
+    #     return [image.image.url for image in images]
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image')
+
+
+class ProductColorImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'color')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Color, ColorAdmin)
+admin.site.register(Size, SizeAdmin)
+admin.site.register(ProductSizeColor, ProductSizeColorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
+admin.site.register(ProductColorImage, ProductColorImageAdmin)
--- backend/src/apps/store/models.py
+++ backend/src/apps/store/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.utils.text import slugify
+from unidecode import unidecode
+
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=125)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(unidecode(self.name))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    old_price = models.IntegerField(null=True, blank=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{self.id}-{unidecode(self.name)}")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Color(models.Model):
+    color_image = models.ImageField(upload_to='color_images/', null=True, default=None)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Size(models.Model):
+    size = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.size
+
+
+class ProductSizeColor(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    color = models.ForeignKey(Color, default=None, on_delete=models.PROTECT)
+    size = models.ForeignKey(Size, default=None, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=0, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.product} - {self.color} - {self.size}"
+
+
+class ProductColorImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    color = models.ForeignKey(Color, default=None, on_delete=models.SET_DEFAULT)
+    image = models.ManyToManyField("ProductImage")
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return self.image.url
