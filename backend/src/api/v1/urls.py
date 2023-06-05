6e9de65c99fc06eb5f28afc4818a99750cc2e206
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-05 13:14:35 +0300

    Update urls.py

--- backend/src/api/v1/urls.py
+++ backend/src/api/v1/urls.py
@@ -4,6 +4,8 @@ from .store.views import *
 
 urlpatterns = [
     path('products/', get_products),
+    path('products/<slug:category_slug>/', get_products),
     path('categories/', get_categories),
-    path('product/<slug:product_slug>/<slug:color_slug>/', get_product),
+    path('products/<slug:product_slug>/<slug:color_slug>/', get_product),
+
 ]
