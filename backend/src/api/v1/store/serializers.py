7378f41fac0ef1edc5bf6b454f783a09924b1a4b
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-05 11:17:02 +0300

    Update serializers.py

--- backend/src/api/v1/store/serializers.py
+++ backend/src/api/v1/store/serializers.py
@@ -3,15 +3,15 @@ from apps.store.models import ProductColorImage, ProductSizeColor, Category
 
 
 class GetProductsSerializer(serializers.ModelSerializer):
-    product_name = serializers.CharField(source='product.name')
-    product_price = serializers.IntegerField(source='product.price')
-    product_old_price = serializers.IntegerField(source='product.old_price')
+    name = serializers.CharField(source='product.name')
+    price = serializers.IntegerField(source='product.price')
+    old_price = serializers.IntegerField(source='product.old_price')
     color_slug = serializers.CharField(source='color.slug')
     product_slug = serializers.CharField(source='product.slug')
-    image = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()
 
     @staticmethod
-    def get_image(obj):
+    def get_image_url(obj):
         product_image = obj.image.first()  # Assuming image is a related manager
         if product_image:
             return product_image.image.url
@@ -20,12 +20,12 @@ class GetProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductColorImage
         fields = (
-            'product_name',
-            'product_price',
-            'product_old_price',
+            'name',
+            'price',
+            'old_price',
             'color_slug',
             'product_slug',
-            'image',
+            'image_url',
         )
 
 
