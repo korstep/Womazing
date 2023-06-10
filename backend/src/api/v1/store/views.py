283fb4bf1dbcc8050e77a68eaee9710112e9b194
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-10 13:15:46 +0300

    Update views.py

--- backend/src/api/v1/store/views.py
+++ backend/src/api/v1/store/views.py
@@ -3,11 +3,13 @@ from rest_framework.decorators import api_view
 from apps.store.models import ProductColorImage, ProductSizeColor, Category
 from .serializers import GetProductsSerializer, GetCategorySerializer
 from django.db.models import F
-from django.db.models import OuterRef, Exists
-from .utils import get_products_data
+from .utils import get_products_data, products_on_sale, similar_products
 from rest_framework import status
 
+# ! Сделать urls в api  v1, v2
+# ! Реализовать сортировку в products
 
+# было бы не плохо стандартизировать ошибки или возможно какие-то тесты.
 @api_view(['GET'])
 def get_products(request, category_slug=None):
     p = get_products_data(category_slug=category_slug,
@@ -26,15 +28,12 @@ def get_categories(request):
     return Response(serializer.data)
 
 
+# разрезать этот ужас я читаймые части
 @api_view(['GET'])
 def get_product(request, product_slug, color_slug):
     p = ProductSizeColor.objects.select_related('product', 'color', 'size')
-    pci = ProductColorImage.objects.select_related('product', 'color')
-    product_size_color_exists = Exists(p.filter(
-        product=OuterRef('product'),
-        color=OuterRef('color')
-    ))
-    pci = pci.filter(product_size_color_exists)
+
+    pci = products_on_sale()
     colors = p.filter(product__slug=product_slug).values(name=F('color__name'),
                                                          slug=F('color__slug'),
                                                          imageUrl=F('color__color_image')).distinct('color__id')
@@ -45,22 +44,20 @@ def get_product(request, product_slug, color_slug):
         error_data = {'error': 'Product not found'}
         return Response(error_data, status=status.HTTP_404_NOT_FOUND)
 
-    matching_images = pci.filter(product=p.first().product, color=p.first().color).annotate(
-        imageUrl=F('image__image')
-    ).values('imageUrl')
-    matching_images = matching_images.values_list('imageUrl', flat=True)
-    images = list(matching_images)
-    sizes = p.values('size__size', 'quantity')
-    sizes_data = {size['size__size']: size['quantity'] for size in sizes}
+    images = pci.filter(product=p.first().product, color=p.first().color).values_list('image__image', flat=True)
+    sizes_data = {size['size__size']: size['quantity'] for size in p.values('size__size', 'quantity')}
     firs_element_p = p[0]
 
     response_data = {
         'name': firs_element_p.product.name,
+        'slug': firs_element_p.product.slug,
         'price': firs_element_p.product.price,
         'oldPrice': firs_element_p.product.old_price,
-        'selectedColor': firs_element_p.color.pk,
+        'selectedColor': firs_element_p.color.slug,
         'colors': colors,
         'images': images,
         'sizes': sizes_data,
+        'similar': GetProductsSerializer(
+            similar_products(firs_element_p.product.pk, firs_element_p.product.category.pk), many=True).data
     }
     return Response(response_data)
