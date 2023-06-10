4a3d92b0009e9decf65f538456d2145b3e7df49a
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-10 10:13:55 +0300

    Created the products_on_sale function
    This function retrieves the products that are currently on sale.

--- backend/src/api/v1/store/utils.py
+++ backend/src/api/v1/store/utils.py
@@ -4,13 +4,18 @@ from django.db.models import Q
 
 
 def get_products_data(category_slug=None, count=None):
+    products = products_on_sale()
+    if category_slug:
+        products = products.filter(product__category__slug=category_slug)
+    if count:
+        products = products.order_by('-product_id').distinct('product_id')[:int(count)]
+    return products
+
+
+def products_on_sale():
     product_ids = ProductSizeColor.objects.values_list('product_id', flat=True)
     color_ids = ProductSizeColor.objects.values_list('color_id', flat=True)
 
-    p = ProductColorImage.objects.select_related('product', 'color')
-    p = p.filter(Q(product_id__in=Subquery(product_ids)) & Q(color_id__in=Subquery(color_ids)))
-    if category_slug:
-        p = p.filter(product__category__slug=category_slug)
-    if count:
-        p = p.order_by('-product_id').distinct('product_id')[:int(count)]
-    return p
+    products = ProductColorImage.objects.select_related('product', 'color')
+    products = products.filter(Q(product_id__in=Subquery(product_ids)) & Q(color_id__in=Subquery(color_ids)))
+    return products
