2e73875f736a88d61c8895124d6798b7f1d1a8d8
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-12 12:34:05 +0300

    Added sorting to get_products_data and created create_error_404

--- backend/src/api/v1/store/utils.py
+++ backend/src/api/v1/store/utils.py
@@ -1,14 +1,34 @@
+from rest_framework import status
+
 from apps.store.models import ProductColorImage, ProductSizeColor
 from django.db.models import Subquery
 from django.db.models import Q
+from rest_framework.response import Response
+
+sort_by_settings = {
+    '0': '-product_id',
+    '1': 'product__price',
+    '2': '-product__price'
+}
+
+default_sort_by = '0'
 
 
-def get_products_data(category_slug=None, count=None):
+def get_products_data(category_slug=None, count=None, sort_by=None):
     products = products_on_sale()
     if category_slug:
         products = products.filter(product__category__slug=category_slug)
+
+    if sort_by and sort_by in sort_by_settings:
+        sort_field = sort_by_settings[str(sort_by)]
+        products = products.order_by(sort_field)
+    else:
+        sort_field = sort_by_settings[default_sort_by]
+        products = products.order_by(sort_field)
+
     if count:
-        products = products.order_by('-product_id').distinct('product_id')[:int(count)]
+        products = products.distinct('product_id')[:int(count)]
+
     return products
 
 
@@ -25,3 +45,8 @@ def similar_products(product_id, category_id):
     products = products_on_sale().exclude(product__id=product_id)
     products = products.filter(product__category_id=category_id)
     return products
+
+
+def create_error_404(error_message='Not found'):
+    error_data = {'error': error_message}
+    return Response(error_data, status=status.HTTP_404_NOT_FOUND)
