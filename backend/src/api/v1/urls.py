d8b43a32c37e79365e2612944c1b3e05e7c0bc69
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-12 12:07:44 +0300

    Update urls.py

--- backend/src/api/v1/urls.py
+++ backend/src/api/v1/urls.py
@@ -1,5 +1,4 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from .store.views import *
 
 urlpatterns = [
@@ -7,5 +6,4 @@ urlpatterns = [
     path('products/<slug:category_slug>/', get_products),
     path('categories/', get_categories),
     path('products/<slug:product_slug>/<slug:color_slug>/', get_product),
-
 ]
