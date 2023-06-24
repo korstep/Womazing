70853de1ae6123b57a530f7f2416aba4e906a723
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-24 16:32:46 +0300

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,27 @@
-import { createStore } from 'vuex'
+import axios from "axios"
+import { createStore } from "vuex"
 
 export default createStore({
+  actions: {
+    async getCategoriesFromBackend({ commit, getters }) {
+      const catg = await axios
+        .get(`${getters.getBackendUrl}v1/categories/`)
+        .then((response) => response.data)
+      commit("setCategories", catg)
+    },
+  },
   state: {
+    backendBaseUrl: "http://127.0.0.1:8000/api/",
+    categories: null,
   },
   getters: {
+    getBackendUrl: (state) => state.backendBaseUrl,
+    getCategories: (state) => state.categories,
   },
   mutations: {
+    setCategories(state, categories) {
+      state.categories = categories
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
+  modules: {},
 })
