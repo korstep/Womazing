041690d3a5e4dfb5be0ae451cee25354798d2eb5
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-25 21:19:51 +0300

    Create TheContacts.vue

---
+++ frontend/src/components/TheContacts.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="contacts">
+    <div class="container">
+      <div class="contacts__body">
+        <div class="contacts__item">
+          <span class="contacts__item-heading">Телефон</span>
+          <h4 class="contacts__item-contact">+7 (495) 823-54-12</h4>
+        </div>
+        <div class="contacts__item">
+          <span class="contacts__item-heading">E-mail</span>
+          <h4 class="contacts__item-contact">info@sitename.com</h4>
+        </div>
+        <div class="contacts__item">
+          <span class="contacts__item-heading">Адрес</span>
+          <h4 class="contacts__item-contact">
+            г. Москва, 3-я улица Строителей, 25
+          </h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+.contacts {
+  &__body {
+    display: flex;
+    column-gap: 100px;
+  }
+  &__item {
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+  }
+  &__item-heading {
+    @include text;
+  }
+  &__item-contact {
+    @include heading-h4;
+  }
+}
+</style>
