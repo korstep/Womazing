f95a9ad24a1d1e887bf9e55bc8de364949dcac8f
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-26 11:05:50 +0300

    Update ContactsView.vue

--- frontend/src/views/ContactsView.vue
+++ frontend/src/views/ContactsView.vue
@@ -1,10 +1,14 @@
 <template>
+  <store-location-map />
   <the-contacts />
+  <contact-us-form />
 </template>
 <script>
 import TheContacts from "@/components/TheContacts.vue"
+import StoreLocationMap from "@/components/StoreLocationMap.vue"
+import ContactUsForm from "@/components/ContactUsForm.vue"
 export default {
-  components: { TheContacts },
+  components: { TheContacts, StoreLocationMap, ContactUsForm },
 }
 </script>
 <style lang="scss"></style>
