6b37e5b99c1111b659addafabd6648a011ebd499
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-26 11:06:13 +0300

    Create StoreLocationMap.vue

---
+++ frontend/src/components/StoreLocationMap.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="map">
+    <div class="container">
+      <div class="map__body">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2042.0993978626216!2d30.106221176972536!3d59.78089267479393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46963d22c550fee3%3A0x15e117dcadd8c1b7!2sPivnoy%20Larek!5e1!3m2!1suk!2sua!4v1682142720109!5m2!1suk!2sua"
+          width="100%"
+          height="480"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss"></style>
