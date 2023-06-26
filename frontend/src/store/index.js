2e76ade13fe13750ca10f1151fb06bee1ac9868e
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-26 18:51:19 +0300

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,26 +3,55 @@ import { createStore } from "vuex"
 
 export default createStore({
   actions: {
-    async getCategoriesFromBackend({ commit, getters }) {
-      const catg = await axios
+    async createCategories({ commit, getters }) {
+      const categories = await axios
         .get(`${getters.getBackendUrl}v1/categories/`)
         .then((response) => response.data)
-      commit("setCategories", catg)
+      commit("setCategories", categories)
+    },
+    async createCatalog({ commit, getters }, sortBy = "") {
+      const catalog = await axios
+        .get(`${getters.getBackendUrl}v1/products/?sort_by=${sortBy}`)
+        .then((response) => response.data)
+      commit("setCatalog", catalog)
+    },
+    async createCatalogCategory({ commit, getters }, context) {
+      console.log(context.category, context.sortBy)
+      const categories = getters.getCategories.map((item) => item.slug)
+      if (getters.getCategories && categories.includes(context.category)) {
+        try {
+          const catalog = await axios
+            .get(
+              `${getters.getBackendUrl}v1/products/${context.category}/?sort_by=${context.sortBy}`
+            )
+            .then((response) => response.data)
+          commit("setCatalog", catalog)
+        } catch (error) {
+          commit("setCatalog", null)
+        }
+      } else {
+        return
+      }
     },
   },
   state: {
     backendBaseUrl: "http://127.0.0.1:8000",
     backendApiUrl: "http://127.0.0.1:8000/api/",
     categories: null,
+    catalog: null,
   },
   getters: {
     getBackendBaseUrl: (state) => state.backendBaseUrl,
     getBackendUrl: (state) => state.backendApiUrl,
     getCategories: (state) => state.categories,
+    getCatalog: (state) => state.catalog,
   },
   mutations: {
     setCategories(state, categories) {
       state.categories = categories
+    },
+    setCatalog(state, catalog) {
+      state.catalog = catalog
     },
   },
   modules: {},
