e4b533c4547f3957b39f662d9ce5bce6a5d55364
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-26 12:44:03 +0300

    Update StoreView.vue

--- frontend/src/views/StoreView.vue
+++ frontend/src/views/StoreView.vue
@@ -1,5 +1,25 @@
-<template><div></div></template>
+<template>
+  <ProductCategories />
+  <ProductsCatalog />
+</template>
 <script>
-export default {}
+import ProductCategories from "@/components/ProductsCategories.vue"
+import ProductsCatalog from "@/components/ProductsCatalog.vue"
+import { mapActions, mapGetters } from "vuex"
+export default {
+  components: { ProductCategories, ProductsCatalog },
+  computed: {
+    ...mapGetters(["getCatalog"]),
+  },
+  mounted: async function () {
+    console.log("hello")
+    if (!this.getCatalog) {
+      await this.createCatalog()
+    }
+  },
+  methods: {
+    ...mapActions(["createCatalog"]),
+  },
+}
 </script>
 <style lang="scss"></style>
