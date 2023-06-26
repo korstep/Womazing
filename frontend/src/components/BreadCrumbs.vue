af8393e1a993918ac885eb1c702dac5df2ef587f
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-26 18:51:12 +0300

    Update BreadCrumbs.vue

--- frontend/src/components/BreadCrumbs.vue
+++ frontend/src/components/BreadCrumbs.vue
@@ -1,5 +1,29 @@
-<template></template>
+<template>
+  <div class="crumbs">
+    <div class="container">
+      <div class="crumbs__body">
+        <span class="crumbs__curr">Магазин</span>
+        <div class="crumbs__path">Главная - Магазин</div>
+      </div>
+    </div>
+  </div>
+</template>
 <script>
 export default {}
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.crumbs {
+  margin-top: 190px;
+  &__body {
+    display: flex;
+    flex-direction: column;
+    row-gap: 25px;
+  }
+  &__curr {
+    @include heading-h1;
+  }
+  &__path {
+    @include text;
+  }
+}
+</style>
