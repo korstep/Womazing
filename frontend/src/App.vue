98c7fbef5a9ce2b349c163b659f232addf955635
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-26 18:51:09 +0300

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
   <div class="wrapper">
     <the-header />
     <main class="main">
+      <bread-crumbs v-if="shouldShowComponent" />
       <router-view />
     </main>
     <the-footer />
@@ -11,24 +12,29 @@
 <script>
 import { mapActions, mapGetters } from "vuex"
 
-import TheHeader from "./components/TheHeader.vue"
-import TheFooter from "./components/TheFooter.vue"
+import TheHeader from "@/components/TheHeader.vue"
+import TheFooter from "@/components/TheFooter.vue"
+import BreadCrumbs from "@/components/BreadCrumbs.vue"
 
 export default {
   name: "App",
   components: {
     TheHeader,
     TheFooter,
+    BreadCrumbs,
   },
   computed: {
-    ...mapGetters(["getBackendUrl", "getCategories"]),
+    ...mapGetters(["getCategories"]),
+    shouldShowComponent() {
+      return this.$route.name !== "home"
+    },
   },
-  created() {
-    this.getCategoriesFromBackend()
+  created: async function () {
+    await this.createCategories()
   },
 
   methods: {
-    ...mapActions(["getCategoriesFromBackend"]),
+    ...mapActions(["createCategories"]),
   },
 }
 </script>
