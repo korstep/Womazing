93b7d4e8e2a62a797a7400ec41a8fefebb8701b9
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-26 11:06:07 +0300

    Create ContactUsForm.vue

---
+++ frontend/src/components/ContactUsForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="write-us">
+    <div class="container">
+      <div class="write-us__body">
+        <h3 class="write-us__title">Напишите нам</h3>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss"></style>
