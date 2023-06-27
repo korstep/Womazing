4b5547f2464613063e4f531c6b4d9dfaa89fc91f
Steve <kornilov.stepan11.s06@gmail.com>
2023-06-27 21:44:36 +0300

    Update Productview.vue

--- frontend/src/views/Productview.vue
+++ frontend/src/views/Productview.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>hello</div>
+  <ProductDetail />
 </template>
 <script>
+import ProductDetail from "@/components/ProductDetail.vue"
+import { mapActions } from "vuex"
 export default {
-  computed: {},
-  methods: {},
+  components: { ProductDetail },
+  mounted: function () {
+    this.createProductDetail(this.getContext)
+  },
+  computed: {
+    getContext() {
+      return {
+        productSlug: `${this.$route.params.productSlug}`,
+        colorSlug: `${this.$route.params.colorSlug}`,
+      }
+    },
+  },
+  methods: {
+    ...mapActions(["createProductDetail"]),
+  },
 }
 </script>
 <style lang="scss"></style>
