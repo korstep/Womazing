82acf485ba9ec6d0c8e8105fc813785ef4cdcb0f
Steve <kornilov.stepan11.s06@gmail.com>
2023-07-07 14:11:12 +0300

    implemented the pagination

--- frontend/src/components/ProductsCatalog.vue
+++ frontend/src/components/ProductsCatalog.vue
@@ -4,7 +4,7 @@
       <div v-if="getCatalog" class="catalog__body">
         <section v-if="getCatalog" class="products">
           <product-cart
-            v-for="(product, index) in getCatalog"
+            v-for="(product, index) in paginatedCatalog"
             :key="index"
             :name="product.name"
             :price="product.price"
@@ -14,6 +14,33 @@
             :imagePath="product.image"
           />
         </section>
+        <div class="catalog__pagination-info">
+          Показано: {{ currentRange }} из {{ getCatalog.length }} товаров
+        </div>
+        <div class="catalog__pagination">
+          <img
+            @click="goToPrevPage"
+            v-if="currentPage !== 1"
+            class="catalog__arrow catalog__arrow_prev"
+            src="@/assets/media/icons/arrow-r.svg"
+            alt="<-"
+          />
+          <span
+            v-for="page in totalPages"
+            :key="page"
+            @click="goToPage(page)"
+            :class="{ catalog__page_active: page === currentPage }"
+            class="catalog__page"
+            >{{ page }}</span
+          >
+          <img
+            @click="goToNextPage"
+            v-if="currentPage !== totalPages"
+            class="catalog__arrow catalog__arrow_next"
+            src="@/assets/media/icons/arrow-r.svg"
+            alt="->"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -26,17 +53,45 @@ export default {
     ProductCart,
   },
   data() {
-    return {}
+    return {
+      currentPage: 1,
+      itemsPerPage: 9,
+    }
   },
   mounted: async function () {
     await this.manageCatalog()
   },
   computed: {
     ...mapGetters(["getCatalog"]),
+    paginatedCatalog() {
+      const start = (this.currentPage - 1) * this.itemsPerPage
+      const end = start + this.itemsPerPage
+      return this.getCatalog.slice(start, end)
+    },
+    totalPages() {
+      return Math.ceil(this.getCatalog.length / this.itemsPerPage)
+    },
+    currentRange() {
+      const end = this.currentPage * this.itemsPerPage
+      return Math.min(end, this.getCatalog.length)
+    },
   },
 
   methods: {
     ...mapActions(["createCatalogCategory", "createCatalog"]),
+    goToNextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++
+      }
+    },
+    goToPage(page) {
+      this.currentPage = page
+    },
+    goToPrevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--
+      }
+    },
     async manageCatalog() {
       if (this.$route.name == "store") {
         await this.createCatalog()
@@ -56,15 +111,55 @@ export default {
 }
 </script>
 <style lang="scss">
-.products {
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  row-gap: 30px;
-  column-gap: 30px;
-  margin-bottom: 65px;
-}
-.product-cart {
-  width: calc(33.33% - 20px);
+.catalog {
+  &__body {
+    display: flex;
+    flex-direction: column;
+    row-gap: 65px;
+  }
+  .products {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    row-gap: 30px;
+    column-gap: 30px;
+    margin-bottom: 65px;
+  }
+  .product-cart {
+    width: calc(33.33% - 20px);
+  }
+  &__pagination {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    column-gap: 15px;
+  }
+  &__page {
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    background-color: #fff;
+    border: 1px solid #000;
+    transition: background-color 0.5s ease;
+    // transition: color 0.5s ease;
+  }
+  &__page_active {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    background-color: #000;
+    color: #fff;
+  }
+  &__arrow {
+    width: 25px;
+  }
+  &__arrow_prev {
+    transform: rotate(180deg);
+  }
 }
 </style>
