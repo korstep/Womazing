4aa1d396eb3eecd680609156db47d00c8789236c
Steve <kornilov.stepan11.s06@gmail.com>
2023-07-07 14:11:33 +0300

    Create CartTotal.vue

---
+++ frontend/src/components/CartTotal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="total">
+    <div class="container">
+      <div class="total__body">
+        <div class="total__total">
+          <span>Итого:</span>
+          <span>${{ getTotal }}</span>
+        </div>
+        <button class="total__button">Оформить заказ</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex"
+
+export default {
+  computed: {
+    ...mapGetters(["getTotal"]),
+  },
+}
+</script>
+<style lang="scss">
+.total {
+  &__body {
+    display: flex;
+    justify-content: end;
+    column-gap: 15px;
+  }
+  &__total {
+    @include heading-h3;
+    padding-right: 30px;
+    padding-left: 30px;
+    background: #f1eadc;
+    display: flex;
+    align-items: center;
+    column-gap: 70px;
+  }
+  &__button {
+    @include button-filled;
+  }
+}
+</style>
