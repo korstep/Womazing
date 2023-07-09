276ae142e82f7c79d5a5598e47d38cb7c4a06e77
Steve <kornilov.stepan11.s06@gmail.com>
2023-07-09 13:45:11 +0300

    Create layouts

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="main">
+    <router-view></router-view>
+  </main>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+.main {
+  flex: 1 1 auto;
+  padding-top: 95px;
+  padding-bottom: 130px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 130px;
+}
+</style>
